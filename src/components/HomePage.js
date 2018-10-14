53d7187bcaed05df4e369e885792eb301bb03eab
Sonam <sonamklein@gmail.com>
2018-10-14 13:09:37 -0400

    commit before restyling

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,11 +1,18 @@
 import React from 'react';
 
 const HomePage = () => (
-  <div>
-    <h1>Sonam Klein</h1>
-    <h1 className = "makeGrey">&#123;Front End,</h1>
-    <h1 className = "makeGrey">Back End&#125;</h1>
-    <h1>Full Stack Developer</h1>
+  <div className = "banner">
+    <div>
+      <h1>Sonam Klein</h1>
+      <h1 className = "makeGrey">&#123;Front End,</h1>
+      <h1 className = "makeGrey">Back End&#125;</h1>
+      <h1>Full Stack Developer</h1>
+    </div>
+    <div>
+      <button className ="mainButton">Learn More</button>
+      <br/>
+      <button className = "mainButton">Contact Me</button>
+    </div>
   </div>
 );
 
