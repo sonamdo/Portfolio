3deca34b860f6b8e48e7faa55ef0bb6178e6c219
Sonam <sonamklein@gmail.com>
2018-10-24 14:05:23 -0400

    updated logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = () => (
   <header>
   <div id = "navbarContainer">
     <ul className = "flex-container" id = "navbar">
-        <h2 className = "align-left">SK</h2>
+        <h2 className = "align-left"><img src = {require("../images/logoSmall.png")} id = "logoSmall"/></h2>
         <li activeClassName="is-active" exact={true} className = "headerLink"><a href="#homeAnchor">Home</a></li>
         <li activeClassName="is-active" exact={true} className = "headerLink"><a href="#portfolioAnchor">Portfolio</a></li>
         <li activeClassName="is-active" className = "headerLink"><a href="#skillsAnchor">Skills</a></li>
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -3,7 +3,7 @@ import React from 'react';
 const HomePage = () => (
   <div className = "banner" id = "homeAnchor">
     <div>
-      <h1>Sonam Klein</h1>
+      <img src = {require("../images/logo.png")} id = "logo"/>
       <h1 className = "makeGrey">Ecommerce Specialist</h1>
       <h1>Full Stack Developer</h1>
     </div>
